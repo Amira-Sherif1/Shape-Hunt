a448b593ddd727ea2ab3c08af6b665d25ce57ebf
Amira-Sherif1 <s-amira.nassef@zewailcity.edu.eg>
2024-03-22 21:13:58 +0200

    demo

---
+++ demo.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include <sstream>
+#include <cmath>
+
+#include "CMUgraphicsLib\CMUgraphics.h"
+#include "CMUgraphicsLib\auxil.h"	// where Pause is found
+
+void drawhome(window& w, color c, int x, int y, int size) {
+	w.SetPen(c);
+	w.SetBrush(c);
+
+	w.DrawRectangle(x, y, x + size, y+size, FILLED);
+
+	w.SetBrush(BLUE);
+	w.DrawTriangle(x, y, x + size, y, x + size / 2, y - size / 2, FILLED);
+}
+
+
+int main()
+{
+
+	//Create an object of controller
+/*	game Game;
+
+	Game.run();
+	return 0;*/
+
+	window w(1200, 700, 5, 5);
+
+	drawhome(w, RED, 700, 100, 60);
+	w.SetBrush(BLUE);
+	//w.DrawRectangle(100, 100, 300, 100 + 150, FILLED);
+	
+}
--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
 #include "game.h"
 
+void drawhome(window& w, color c, int x, int y, int size) {
+	w.SetPen(c);
+	w.DrawRectangle(x, y, x+size, y, FILLED,size,size*1.5);
+	w.DrawTriangle(x, y, x+size, y,x+ size / 2, y+size / 2, FILLED);
+}
+
 
 int main()
 {
 
 	//Create an object of controller
-	game Game;
+/*	game Game;
 	
 	Game.run();
+	return 0;*/
 
+	window w(1200, 700, 5, 5);
+
+	drawhome(w, RED, 50, 50, 20);
 	
-	return 0;
+	
+	
 }
-
